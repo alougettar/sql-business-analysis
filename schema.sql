17edeb932dcd72ae53ed9449e40d0531099a0a62
Alejandra Gettar <146187082+alougettar@users.noreply.github.com>
2026-01-02 16:22:28 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,24 @@
+-- Users table
+CREATE TABLE users (
+    user_id INT,
+    signup_date DATE,
+    country TEXT
+);
+
+-- Subscriptions table
+CREATE TABLE subscriptions (
+    subscription_id INT,
+    user_id INT,
+    start_date DATE,
+    end_date DATE,
+    plan TEXT
+);
+
+-- Payments table
+CREATE TABLE payments (
+    payment_id INT,
+    user_id INT,
+    payment_date DATE,
+    amount DECIMAL
+);
 
